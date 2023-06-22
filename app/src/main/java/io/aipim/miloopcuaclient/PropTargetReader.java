27b9fabeca9ba1931a82bea097c153ffe565351b
Abiria <abiria.dev@gmail.com>
2023-06-22 14:53:22 +0900

    chore: remove unused import

--- app/src/main/java/io/aipim/miloopcuaclient/PropTargetReader.java
+++ app/src/main/java/io/aipim/miloopcuaclient/PropTargetReader.java
@@ -1,8 +1,6 @@
 package io.aipim.miloopcuaclient;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Properties;
 
